53aed2f49cd5954334518238d4df0ffc8d6b0dc1
C. Jordan Ball III <cjordanball@gmail.com>
2017-05-25 08:48:37 -0400

    add problem 8

---
+++ spec/problemE008.spec.js
@@ -0,0 +1,16 @@
+const { CheckNums } = require('../problems/easy/problemE008');
+
+describe('CheckNums', () => {
+	it('should return "-1" if the numbers are equal to each other', () => {
+		expect(CheckNums(5, 5)).toBe('-1');
+		expect(CheckNums(0, 0)).toBe('-1');
+	});
+	it('should return true if second param is greater', () => {
+		expect(CheckNums(3, 10)).toBe(true);
+		expect(CheckNums(-10, -3)).toBe(true);
+	});
+	it('should return false if first param is greater', () => {
+		expect(CheckNums(10, 3)).toBe(false);
+		expect(CheckNums(-3, -10)).toBe(false);
+	});
+});
