b4537ba5688e41b89e57adda1d59c586925eaa85
C. Jordan Ball III <cjordanball@gmail.com>
2017-07-26 17:27:48 -0400

    problem 16

---
+++ spec/problemE016.spec.js
@@ -0,0 +1,25 @@
+const { ArithGeo, helpers: { ArithTest, GeoTest } } = require('../problems/easy/problemE016');
+
+describe('ArithGeo', () => {
+	describe('ArithTest', () => {
+		it('should test whether a series is an Arithmetic progression', () => {
+			expect(ArithTest([1, 2, 3, 4, 5])).toBe('Arithmetic');
+			expect(ArithTest([1, 2, 5, 8, 9])).toBe(false);
+		});
+	});
+	describe('GeoTest', () => {
+		it('should test whether a series is an geometric progression', () => {
+			expect(GeoTest([1, 2, 4, 8, 16])).toBe('Geometric');
+			expect(GeoTest([1, 2, 5, 8])).toBe(false);
+		});
+		it('should handle a case where the multiplier is negative', () => {
+			expect(GeoTest([2, -6, 18, -54, 162]));
+		});
+	});
+
+	it('should return what kind of series it is', () => {
+		expect(ArithGeo([1, 2, 4, 8, 16])).toBe('Geometric');
+		expect(ArithGeo([1, 2, 5, 8])).toBe(-1);
+		expect(ArithGeo([1, 5, 9, 13, 17, 21])).toBe('Arithmetic');
+	});
+});
