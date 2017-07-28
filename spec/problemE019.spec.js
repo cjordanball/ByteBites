160d7612543ff432189d81f3c85e0a08b4311d34
C. Jordan Ball III <cjordanball@gmail.com>
2017-07-27 23:18:34 -0400

    problem 19

---
+++ spec/problemE019.spec.js
@@ -0,0 +1,15 @@
+const { SecondGreatLow } = require('../problems/easy/problemE019');
+
+describe('SecondGreatLow', () => {
+	it('should return second lowest and second highest numbers', () => {
+		expect(SecondGreatLow([5, 1, 6, 9, 3, 4])).toBe('3 6');
+	});
+
+	it('should return a two element array in reverse order', () => {
+		expect(SecondGreatLow([3, 4])).toBe('4 3');
+	});
+
+	it('should deal correctly with multiple entries', () => {
+		expect(SecondGreatLow([1, 1, 2, 3, 4, 5, 5, 5, 1000, 1000])).toBe('2 5');
+	});
+});
