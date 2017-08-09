637b94dcf628ad4a25ecac89c95dc0e7ea6c86bf
C. Jordan Ball III <cjordanball@gmail.com>
2017-08-08 23:44:45 -0400

    problem 20

---
+++ spec/problemE020.spec.js
@@ -0,0 +1,15 @@
+const { DivisionStringified } = require('../problems/easy/problemE020');
+
+describe('DivisionStringified', () => {
+	it('should return 3 or fewer digit numbers unchanged', () => {
+		expect(DivisionStringified(54, 18)).toBe('3');
+	});
+
+	it('should truncate remainders', () => {
+		expect(DivisionStringified(314, 9)).toBe('34');
+	});
+
+	it('should deal correctly with commas on large numbers', () => {
+		expect(DivisionStringified(353926003239, 3)).toBe('117,975,334,413');
+	});
+});
