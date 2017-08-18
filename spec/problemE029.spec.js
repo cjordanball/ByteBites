97548a146d3a84e06f11abd052e61a51ab1261ce
C. Jordan Ball III <cjordanball@gmail.com>
2017-08-17 22:31:06 -0400

    problem29

---
+++ spec/problemE029.spec.js
@@ -0,0 +1,16 @@
+const { MultiplicativePersistence, helpers: { arrayPrep } } = require('../problems/easy/problemE029');
+
+describe('MultiplicativePersistence', () => {
+	describe('ArrayPrep', () => {
+		it('returns a summation of integers in a number', () => {
+			expect(arrayPrep(134)).toBe(12);
+			expect(arrayPrep(654321)).toBe(720);
+		});
+	});
+	it('should return the correct answer', () => {
+		expect(MultiplicativePersistence(949584935294)).toBe(2);
+	});
+	it('returns 0 as the answer for 1', () => {
+		expect(MultiplicativePersistence(1)).toBe(0);
+	});
+});
