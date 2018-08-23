746d58ef5aa45bec0cc05ad9823de67153659650
Jordan Ball <jordan@cjordanball.info>
2018-08-22 23:48:30 -0400

    add problem E063

---
+++ spec/problemE063.spec.js
@@ -0,0 +1,7 @@
+const { NumberStream } = require('../problems/easy/E063NumberStream');
+
+describe('NumberStream', () => {
+	it('return an accurate answer', () => {
+		expect(NumberStream('6539923335')).toBe(true);
+	});
+});
