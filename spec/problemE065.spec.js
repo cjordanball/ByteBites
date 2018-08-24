6a9debe1c578975d48b2691b4a3b5edc2994d69c
Jordan Ball <jordan@cjordanball.info>
2018-08-24 15:12:14 -0400

    add problem E065

---
+++ spec/problemE065.spec.js
@@ -0,0 +1,10 @@
+const { DistinctCharacters } = require('../problems/easy/E065DistinctCharacters');
+
+describe('DistinctCharacters', () => {
+	it('returns false if not sufficient', () => {
+		expect(DistinctCharacters('abcdeffffffffffgh')).toBe(false);
+	});
+	it('returns true if sufficient', () => {
+		expect(DistinctCharacters('asdfghjkl;wertyuio')).toBe(true);
+	});
+});
