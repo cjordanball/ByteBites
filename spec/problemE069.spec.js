338394f13f440266046c5135d8b0d5cdcca808f2
Jordan Ball <jordan@cjordanball.info>
2018-08-28 10:04:48 -0400

    add new problem E069

---
+++ spec/problemE069.spec.js
@@ -0,0 +1,10 @@
+const { SimpleEvens } = require('../problems/easy/E069SimpleEvens');
+
+describe('SimpleEvens', () => {
+	it('the correct value for an all even number', () => {
+		expect(SimpleEvens('28684222')).toBe('true');
+	});
+	it('gives the correct value for an odd digit containing number', () => {
+		expect(SimpleEvens('238624086')).toBe('false');
+	});
+});
