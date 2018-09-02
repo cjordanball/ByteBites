935bdc29d6538329a27c6f88e8b17c1477200406
Jordan Ball <jordan@cjordanball.info>
2018-09-01 23:40:22 -0400

    add problem E072

--- spec/problemE030.spec.js
+++ spec/problemE030.spec.js
@@ -1,10 +1,10 @@
-const { OffLineMinimum } = require('../problems/easy/problemE030');
+const { OffLineMinimum } = require('../problems/easy/E030OffLineMinimum');
 
 describe('OffLineMinimum', () => {
 	it('should correctly handle an array of numbers and Es', () => {
-		expect(OffLineMinimum(["5","4","6","E","1","7","E","E","3","2"])).toBe('4,1,5');
+		expect(OffLineMinimum(['5', '4', '6', 'E', '1', '7', 'E', 'E', '3', '2'])).toBe('4,1,5');
 	});
 	it('should handle an initial E', () => {
-		expect(OffLineMinimum(["E","1", "E","12","15","23","E","2","33","E"])).toBe('1,12,2');
+		expect(OffLineMinimum(['E', '1', 'E', '12', '15', '23', 'E', '2', '33', 'E'])).toBe('1,12,2');
 	});
 });
---
+++ spec/problemE072.spec.js
@@ -0,0 +1,7 @@
+const { StringMerge } = require('../problems/easy/E072StringMerge');
+
+describe('StringMerge', () => {
+	it('returns a correct true value', () => {
+		expect(StringMerge('abc1*kyoo')).toBe('akbyco1o');
+	});
+});
