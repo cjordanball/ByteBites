b75b778688150660970ad02634ed87de0ba6a0dd
Jordan Ball <jordan@cjordanball.info>
2018-09-02 22:59:57 -0400

    add problem M075

---
+++ problems/medium/M075Primes.js
@@ -0,0 +1,36 @@
+/*
+Using the JavaScript language, have the function Primes(num) take the num parameter being
+passed and return the string true if the parameter is a prime number, otherwise return the
+string false. The range will be between 1 and 2^16.
+*/
+
+const info = {
+	name: ['StringPeriods'],
+	number: 75,
+	level: 'medium',
+	methods: ['Math.floor()', 'Math.sqrt()'],
+	concepts: ['for-loop', 'if-statement', 'switch statement', 'remainder operator']
+};
+
+const Primes = (num) => {
+	switch (num) {
+		case 1:
+			return 'false';
+		case 2:
+			return 'true';
+		default: {
+			const pivot = Math.floor(Math.sqrt(num));
+			for (let i = 2; i <= pivot; i++) {
+				if (!(num % i)) {
+					return 'false';
+				}
+			}
+			return 'true';
+		}
+	}
+};
+
+module.exports = {
+	Primes,
+	info
+};
